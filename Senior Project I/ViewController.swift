23a660fac66884a2b6257e43a42e21670d15bc20
Jarukorn Thuengjitvilas <jarukorn@jarukorns-imac.local>
2018-03-18 23:43:22 +0700

    Initial Commit

---
+++ Senior Project I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Senior Project I
+//
+//  Created by Jarukorn Thuengjitvilas on 18/3/2561 BE.
+//  Copyright © 2561 Jarukorn Thuengjitvilas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
