9083506cebd92ac7119b339e27c0249f8fc2a4c1
Jarukorn Thuengjitvilas <jarukorn@jarukorns-mbp.true.true>
2018-05-30 16:37:57 +0700

    Fix getProjectList

--- Senior Project I/Classes/API.swift
+++ Senior Project I/Classes/API.swift
@@ -12,11 +12,34 @@ import Alamofire
 let url = URL(string: "https://traxapplication.visualstudio.com/DefaultCollection/")
 let apiVersion = "2.0"
 
-func getProjectList(){
+struct ProjectFromTFS {
+    var id: String?
+    var name: String?
+}
+
+func getProjectList(result: @escaping ([ProjectFromTFS]) -> Void) {
+    
+    var projectList = [ProjectFromTFS]()
     let headers: HTTPHeaders = ["Authorization": "Basic Om90dGw1d2xlc2U1dmJwZWV1Y2oyNHJrZnVkYTN5Y3ljdWJ4Zmh6c25qejRzNGxoM3RieWE="]
     Alamofire.request("\(url!)_apis/projects?api-version=\(apiVersion)",headers: headers).responseJSON { (response) in
-        print(response.result.value!)
-    }
+        if (response.result.value != nil) {
+            if let json = response.result.value as? [String:AnyObject] {
+                let value = json["value"] as? [[String:AnyObject]]
+                for obj in value! {
+                    let id = obj["id"] as? String
+                    let name = obj["name"] as? String
+                    let projectTemp = ProjectFromTFS(id: id, name: name)
+                    projectList.append(projectTemp)
+                }
+                result(projectList)
+            } else {
+
+            }
+
+        } else {
+
+        }
+    }.resume()
 }
 
 //
